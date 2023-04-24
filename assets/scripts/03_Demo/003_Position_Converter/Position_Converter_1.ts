ede5c577ce257626ea954ccc25c11c649efb879f
xww <767412271qq.com>
2023-04-24 14:30:51 +0800

    node->node坐标转换

--- assets/scripts/03_Demo/003_Position_Converter/Position_Converter_1.ts
+++ assets/scripts/03_Demo/003_Position_Converter/Position_Converter_1.ts
@@ -1,11 +1,26 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, instantiate, Node, Prefab, UITransform } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Position_Converter_1')
 export class Position_Converter_1 extends Component {
 
+    @property(Prefab)
+    public nodeLabel: Prefab;
+
+    @property(Node)
+    public group_1_label: Node;
+
+    @property(Node)
+    public group_2: Node;
+
     start() {
 
+    }
+
+    node2NodePosition() {
+        let label = instantiate(this.nodeLabel);
+        label.position = this.group_1_label.position;
+        label.parent = this.group_2;
     }
 
     update(deltaTime: number) {
