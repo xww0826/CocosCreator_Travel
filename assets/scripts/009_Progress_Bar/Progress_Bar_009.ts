b04d30a97a94ca2510f2bd84a337eb41c85d17ae
xuweiwei <122265264@qq.com>
2023-06-04 19:03:13 +0800

    progress bar

--- assets/scripts/009_Progress_Bar/Progress_Bar_009.ts
+++ assets/scripts/009_Progress_Bar/Progress_Bar_009.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, ProgressBar } from 'cc';
+import { _decorator, Component, Node, ProgressBar, Sprite } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Progress_Bar_009')
@@ -8,6 +8,14 @@ export class Progress_Bar_009 extends Component {
     public progressBar_1: ProgressBar;
     @property(ProgressBar)
     public progressBar_2: ProgressBar;
+    @property(ProgressBar)
+    public progressBar_3: ProgressBar;
+    @property(ProgressBar)
+    public progressBar_4: ProgressBar;
+    @property(Sprite)
+    public horizontal: Sprite;
+    @property(Sprite)
+    public ring: Sprite;
 
     private time: number = 0;
 
@@ -26,9 +34,17 @@ export class Progress_Bar_009 extends Component {
         this.setProgress();
     }
 
+    reset() {
+        this.progress = 0;
+    }
+
     private setProgress() {
         this, this.progressBar_1.progress = this.progress;
         this, this.progressBar_2.progress = this.progress;
+        this, this.progressBar_3.progress = this.progress;
+        this, this.progressBar_4.progress = this.progress;
+        this, this.horizontal.fillRange = this.progress;
+        this, this.ring.fillRange = this.progress;
     }
 }
 
